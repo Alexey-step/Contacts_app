f834165ced732b123c963651068d18323307ac76
Aleksey <alexey-step@mail.ru>
2021-06-20 20:08:26 +0500

    refactor (server.js)

--- server.js
+++ server.js
@@ -2,12 +2,10 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const auth = require('json-server-auth');
 const router = jsonServer.router("./db.json");
-const middlewares = jsonServer.defaults({
-  static: "./dist"
-});
+
 const PORT = process.env.PORT || 4000;
 
-server.use(middlewares);
+server.db = router.db
 server.use(auth);
 server.use(router);
 
