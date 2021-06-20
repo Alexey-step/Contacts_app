be37ce2b93315f9c0dbd4c9800e39a9ae9f73bdb
Aleksey <alexey-step@mail.ru>
2021-06-20 18:54:06 +0500

    refactor (server.js)

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("./src/db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4200;
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`)
+});
