13388bed12e23dd1c1f0f731b7bb13541b5eaea1
Aleksey <alexey-step@mail.ru>
2021-06-22 18:51:01 +0500

    fix

--- server.js
+++ server.js
@@ -10,12 +10,6 @@ const PORT = process.env.PORT || 4000;
 
 server.db = router.db
 server.use(middlewares);
-router.render = (req, res) => {
-  if (res.status(404)) {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'))
-  }
-}
-
 server.use(auth);
 server.use(router);
 
