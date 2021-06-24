fd1112780f2ec475320eac524e222cd20157eafc
Aleksey <alexey-step@mail.ru>
2021-06-24 20:00:13 +0500

    refactor (server.js)

--- server.js
+++ server.js
@@ -9,11 +9,13 @@ const middlewares = jsonServer.defaults({
 const router = jsonServer.router('./db.json');
 const PORT = process.env.PORT || 4000;
 
+app.db = router.db;
 app.use(middlewares);
+app.use(auth);
 app.use(jsonServer.rewriter({
   '/api/*': '/$1',
 }))
-app.use(auth, router);
+app.use(router);
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/*', function (req, res) {
