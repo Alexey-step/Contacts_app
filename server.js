67ff54cae13c22a895f6c3250792d635bb96df0c
Aleksey <alexey-step@mail.ru>
2021-07-13 20:33:38 +0500

    fix

--- server.js
+++ server.js
@@ -15,11 +15,12 @@ app.use(auth);
 app.use(jsonServer.rewriter({
   '/api/*': '/$1',
 }))
-app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/*', function (req, res) {
   if (res.statusCode === "404") {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  } else {
+    res.sendFile(req.body)
   }
 });
 
