90993dea28c4c3ca8e85fcab2db047275314cfa2
Aleksey <alexey-step@mail.ru>
2021-07-13 20:48:19 +0500

    fix

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(jsonServer.rewriter({
 }))
 app.use(express.static(path.join(__dirname, 'dist')));
 app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
   console.log(req, res)
 });
 
