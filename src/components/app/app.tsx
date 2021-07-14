318d2498f34424bca107a5cbb6fafe4d3a9860c5
Aleksey <alexey-step@mail.ru>
2021-07-14 20:40:42 +0500

    chore (add custom route)

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -24,7 +24,7 @@ const App: React.FC = () => {
       <Route path={`${AppRoutes.LOGIN}`} exact>
         <Login />
       </Route>
-      <Route>
+      <Route path={`${AppRoutes.NOT_FOUND}`}>
         <NotFound />
       </Route>
     </Switch>
