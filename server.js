f348628f9b2b880b6b7ac034c489cae9360e19a5
Aleksey <alexey-step@mail.ru>
2021-07-14 20:52:13 +0500

    fix

--- server.js
+++ server.js
@@ -16,6 +16,13 @@ app.use(jsonServer.rewriter({
   '/api/*': '/$1',
 }))
 
+app.get('/*', (req, res) => {
+  let url = path.join(__dirname, '../client/build', 'index.html');
+  if (!url.startsWith('/app/'))
+    url = url.substring(1);
+  res.sendFile(url);
+});
+
 app.use(router);
 
 
