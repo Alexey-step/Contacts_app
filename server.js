2488ef055546fcd62d55c545546f570e357aab48
Aleksey <alexey-step@mail.ru>
2021-07-14 20:23:34 +0500

    fix

--- server.js
+++ server.js
@@ -16,14 +16,6 @@ app.use(jsonServer.rewriter({
   '/api/*': '/$1',
 }))
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, 'dist')));
-
-  app.get('/*', (req, res) => {
-      res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-  });
-}
-
 app.use(router);
 
 
