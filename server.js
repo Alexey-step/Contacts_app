2fa54fe9df09dba0b01802b1d80b0354a7658432
Aleksey <alexey-step@mail.ru>
2021-07-14 22:17:43 +0500

    fix

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(auth);
 
 if (process.env.NODE_ENV === "production") {
   app.get("/contacts", (req, res) => {
-    res.jsonp(res)
+    res.jsonp({body: res.locals.contacts})
   });
 
   app.get("*", (req, res) => {
