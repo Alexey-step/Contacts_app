ad5bb6859435a9070eb0ef364b64795158eeba99
Aleksey <alexey-step@mail.ru>
2021-07-14 21:39:07 +0500

    fix

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -24,7 +24,7 @@ const App: React.FC = () => {
       <Route path={`${AppRoutes.LOGIN}`} exact>
         <Login />
       </Route>
-      <Route>
+      <Route path={`${AppRoutes.NOT_FOUND}`} exact>
         <NotFound />
       </Route>
     </Switch>
